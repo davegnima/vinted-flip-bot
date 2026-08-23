8f7bb7b0a12711486845468086dbcb8cc552b386
davegnima <davide.magni.mobile@gmail.com>
2026-08-23 14:39:23 +0200

    Update main_telethon.py

--- main_telethon.py
+++ main_telethon.py
@@ -297,9 +297,9 @@ def check_skip_pre_gemini(listing_info):
         "uniqlo", "h&m",
         "missoni for target", "missoni x target",
     ]
-    for kw in unflippable:
-    if re.search(r'\b' + re.escape(kw) + r'\b', testo_completo):
-        return True, f"[CATEGORIA GENERICA NON FLIPPABILE] Rilevata keyword: {kw}"
+        for kw in unflippable:
+        if re.search(r'\b' + re.escape(kw) + r'\b', testo_completo):
+            return True, f"[CATEGORIA GENERICA NON FLIPPABILE] Rilevata keyword: {kw}"
 
     # 2b. Danno grave dichiarato esplicitamente dal venditore, multilingua
     # (IT/EN/DE/FR/ES/PT -- stessa logica delle categorie: il tracker Vinted
